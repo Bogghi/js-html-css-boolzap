45405bc59b1e1855520501f359bdb1bd2779d4ff
Matteo Borghi <matteosimoneborghi@gmail.com>
2020-09-08 13:59:16 +0200

    workspace setup

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// Esercizio: Boolzapp
+// repo: js-html-css-boolzap
+// Milestone 1
+// Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi) e dall’interlocutore (bianco) assegnando due classi CSS diverse
+// Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e cliccando invia il testo viene aggiunto al thread sopra, come messaggio verde
+// per qualsiasi cosa mi trovate su hotwell! :slightly_smiling_face: buon lavoro!!
